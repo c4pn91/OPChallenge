270663d9f92dc9db3518b7b56a8a91e5d0ff1352
c4pn91 <c4pn.91@gmail.com>
2024-06-22 23:17:55 -0500

    latest changes

--- app/src/main/java/com/c4pn91/opchallenge/util/Constants.kt
+++ app/src/main/java/com/c4pn91/opchallenge/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.c4pn91.opchallenge.util
 
 object Constants {
-    const val API_KEY = "b9105c55264199932e94b85552f60911"
+    const val API_KEY = ""
     const val LANGUAGE = "en-US"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val BASE_IMGS_SMALL = "https://image.tmdb.org/t/p/w200/"
